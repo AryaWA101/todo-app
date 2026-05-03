5bffcf76e2e46cd16e2a5fe80e18685720b9e336
AryaWA101 <aryawibawa1502@gmail.com>
2026-05-03 18:37:48 +0700

    FINAL TUTAM CS SBD 10

---
+++ backend/api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const todoRoutes = require('../routes/todos');
+const authRoutes = require('../routes/auth');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/todos', todoRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => res.json({ message: 'Todo API is running!' }));
+
+let isConnected = false;
+const connectDB = async () => {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+};
+
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
